234a22795a1d6b1530df6bce907cc91eace2afc0
unknown <amansharma.volsol@gmail.com>
2020-04-22 15:59:14 +0530

    theme update signup page

--- login/signup_form.php
+++ login/signup_form.php
@@ -23,56 +23,61 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+defined( 'MOODLE_INTERNAL' ) || die();
 
-require_once($CFG->libdir.'/formslib.php');
-require_once($CFG->dirroot.'/user/profile/lib.php');
-require_once($CFG->dirroot . '/user/editlib.php');
+require_once( $CFG->libdir . '/formslib.php' );
+require_once( $CFG->dirroot . '/user/profile/lib.php' );
+require_once( $CFG->dirroot . '/user/editlib.php' );
 
 class login_signup_form extends moodleform implements renderable, templatable {
-    function definition() {
-        global $USER, $CFG;
+	function definition() {
+		global $USER, $CFG;
 
-        $mform = $this->_form;
+		$mform = $this->_form;
 
 //        $mform->addElement('header', 'createuserandpass', get_string('createuserandpass'), '');
 
+		$mform->addElement('html', '<div class="w-100 amigo-custom-form-group">');
+		profile_signup_fields( $mform, true );
+		$mform->addElement( 'static', 'urldomain', '', '.amigolms.com' );
+		$mform->addElement('html', '</div>');
 
-        $mform->addElement('text', 'username', get_string('username'), 'placeholder="Username" maxlength="100" size="12" autocapitalize="none"');
+		$mform->addElement('html', '<div class="grid grid-col-lg-2 w-100">');
+		$mform->addElement( 'text', 'username', get_string( 'username' ), 'placeholder="Username" maxlength="100" size="12" autocapitalize="none"' );
 //        $mform->addElement('text', 'username', '', 'placeholder="Username" maxlength="100" size="12" autocapitalize="none"');
-        $mform->setType('username', PARAM_RAW);
-        $mform->addRule('username', get_string('missingusername'), 'required', null, 'client');
+		$mform->setType( 'username', PARAM_RAW );
+		$mform->addRule( 'username', get_string( 'missingusername' ), 'required', null, 'client' );
 
 //        if (!empty($CFG->passwordpolicy)){
 //            $mform->addElement('static', 'passwordpolicyinfo', '', print_password_policy());
 //        }
-		
-        $mform->addElement('password', 'password', get_string('password'), 'placeholder="Password" maxlength="32" size="12"');
+
+		$mform->addElement( 'password', 'password', get_string( 'password' ), 'placeholder="Password" maxlength="32" size="12"' );
 //        $mform->addElement('password', 'password', '', 'placeholder="Password" maxlength="32" size="12"');
-        $mform->addHelpButton('password', 'password');
-        $mform->setType('password', core_user::get_property_type('password'));
-        $mform->addRule('password', get_string('missingpassword'), 'required', null, 'client');
+//		$mform->addHelpButton( 'password', 'password' );
+//		$mform->setType( 'password', core_user::get_property_type( 'password' ) );
+		$mform->addRule( 'password', '', 'required', null, 'client' );
 
 //        $mform->addElement('header', 'supplyinfo', get_string('supplyinfo'),'');
-        
-                $namefields = useredit_get_required_name_fields();
 
-        foreach ($namefields as $field) {
-            $mform->addElement('text', $field, get_string($field), 'placeholder="First name"maxlength="100" size="30"');
+		$namefields = useredit_get_required_name_fields();
+
+		foreach ( $namefields as $field ) {
+			$mform->addElement( 'text', $field, get_string( $field ), 'placeholder="First name"maxlength="100" size="30"' );
 //            $mform->addElement('text', $field, '', 'placeholder="First Name"maxlength="100" size="30"');
-            $mform->setType($field, core_user::get_property_type('firstname'));
-            $stringid = 'missing' . $field;
-            if (!get_string_manager()->string_exists($stringid, 'moodle')) {
-                $stringid = 'required';
-            }
-            $mform->addRule($field, get_string($stringid), 'required', null, 'client');
-        }
+			$mform->setType( $field, core_user::get_property_type( 'firstname' ) );
+			$stringid = 'missing' . $field;
+			if ( ! get_string_manager()->string_exists( $stringid, 'moodle' ) ) {
+				$stringid = 'required';
+			}
+			$mform->addRule( $field, get_string( $stringid ), 'required', null, 'client' );
+		}
 
-        $mform->addElement('text', 'email', get_string('email'), 'placeholder="email" maxlength="100" size="30"');
+		$mform->addElement( 'text', 'email', get_string( 'email' ), 'placeholder="email" maxlength="100" size="30"' );
 //        $mform->addElement('text', 'email', '', 'placeholder="Email" maxlength="100" size="25"');
-        $mform->setType('email', core_user::get_property_type('email'));
-        $mform->addRule('email', get_string('missingemail'), 'required', null, 'client');
-        $mform->setForceLtr('email');
+		$mform->setType( 'email', core_user::get_property_type( 'email' ) );
+		$mform->addRule( 'email', get_string( 'missingemail' ), 'required', null, 'client' );
+		$mform->setForceLtr( 'email' );
 
 //        $mform->addElement('text', 'email2', get_string('emailagain'), 'placeholder="email again" maxlength="100" size="25"');
 //        $mform->addElement('text', 'email2', '', 'placeholder="Email Again" maxlength="100" size="25"');
@@ -80,95 +85,100 @@ class login_signup_form extends moodleform implements renderable, templatable {
 //        $mform->addRule('email2', get_string('missingemail'), 'required', null, 'client');
 //        $mform->setForceLtr('email2');
 
+		$mform->addElement( 'text', 'phone1', get_string( 'phone1' ), 'placeholder="Phone Number" maxlength="100" size="30"' );
+//		$mform->setType( 'phone1', core_user::get_property_type( 'phone' ) );
+		$mform->addRule( 'phone1', get_string( 'missinphone1' ), 'required', null, 'client' );
+//		$mform->setForceLtr( 'email' );
+		$mform->addElement('html', '</div>');
 
-        /*$mform->addElement('text', 'city', get_string('city'), 'placeholder="Your City/Town"maxlength="120" size="20"');
-        $mform->setType('city', core_user::get_property_type('city'));
-        if (!empty($CFG->defaultcity)) {
-            $mform->setDefault('city', $CFG->defaultcity);
-        }
+		/*$mform->addElement('text', 'city', get_string('city'), 'placeholder="Your City/Town"maxlength="120" size="20"');
+		$mform->setType('city', core_user::get_property_type('city'));
+		if (!empty($CFG->defaultcity)) {
+			$mform->setDefault('city', $CFG->defaultcity);
+		}
 
-        $country = get_string_manager()->get_list_of_countries();
-        $default_country[''] = get_string('selectacountry');
-        $country = array_merge($default_country, $country);
-        $mform->addElement('select', 'country', get_string('country'), $country);
+		$country = get_string_manager()->get_list_of_countries();
+		$default_country[''] = get_string('selectacountry');
+		$country = array_merge($default_country, $country);
+		$mform->addElement('select', 'country', get_string('country'), $country);
 
-        if( !empty($CFG->country) ){
-            $mform->setDefault('country', $CFG->country);
-        }else{
-            $mform->setDefault('country', '');
-        }*/
+		if( !empty($CFG->country) ){
+			$mform->setDefault('country', $CFG->country);
+		}else{
+			$mform->setDefault('country', '');
+		}*/
 
-        profile_signup_fields($mform,true);
-        $mform->addElement('static', 'urldomain', '', '.amigolms.com');
+		if ( signup_captcha_enabled() ) {
+			$mform->addElement( 'recaptcha', 'recaptcha_element', get_string( 'security_question', 'auth' ) );
+			$mform->addHelpButton( 'recaptcha_element', 'recaptcha', 'auth' );
+			$mform->closeHeaderBefore( 'recaptcha_element' );
+		}
 
-        if (signup_captcha_enabled()) {
-            $mform->addElement('recaptcha', 'recaptcha_element', get_string('security_question', 'auth'));
-            $mform->addHelpButton('recaptcha_element', 'recaptcha', 'auth');
-            $mform->closeHeaderBefore('recaptcha_element');
-        }
+		// Add "Agree to sitepolicy" controls. By default it is a link to the policy text and a checkbox but
+		// it can be implemented differently in custom sitepolicy handlers.
+		$manager = new \core_privacy\local\sitepolicy\manager();
+		$manager->signup_form( $mform );
 
-        // Add "Agree to sitepolicy" controls. By default it is a link to the policy text and a checkbox but
-        // it can be implemented differently in custom sitepolicy handlers.
-        $manager = new \core_privacy\local\sitepolicy\manager();
-        $manager->signup_form($mform);
+		// buttons
+		$this->add_action_buttons( true, get_string( 'createaccount' ) );
 
-        // buttons
-        $this->add_action_buttons(true, get_string('createaccount'));
+	}
 
-    }
+	function definition_after_data() {
+		$mform = $this->_form;
+		$mform->applyFilter( 'username', 'trim' );
 
-    function definition_after_data(){
-        $mform = $this->_form;
-        $mform->applyFilter('username', 'trim');
+		// Trim required name fields.
+		foreach ( useredit_get_required_name_fields() as $field ) {
+			$mform->applyFilter( $field, 'trim' );
+		}
+	}
 
-        // Trim required name fields.
-        foreach (useredit_get_required_name_fields() as $field) {
-            $mform->applyFilter($field, 'trim');
-        }
-    }
+	/**
+	 * Validate user supplied data on the signup form.
+	 *
+	 * @param array $data array of ("fieldname"=>value) of submitted data
+	 * @param array $files array of uploaded files "element_name"=>tmp_file_path
+	 *
+	 * @return array of "element_name"=>"error_description" if there are errors,
+	 *         or an empty array if everything is OK (true allowed for backwards compatibility too).
+	 */
+	public function validation( $data, $files ) {
+		$errors = parent::validation( $data, $files );
 
-    /**
-     * Validate user supplied data on the signup form.
-     *
-     * @param array $data array of ("fieldname"=>value) of submitted data
-     * @param array $files array of uploaded files "element_name"=>tmp_file_path
-     * @return array of "element_name"=>"error_description" if there are errors,
-     *         or an empty array if everything is OK (true allowed for backwards compatibility too).
-     */
-    public function validation($data, $files) {
-        $errors = parent::validation($data, $files);
+		if ( signup_captcha_enabled() ) {
+			$recaptchaelement = $this->_form->getElement( 'recaptcha_element' );
+			if ( ! empty( $this->_form->_submitValues['g-recaptcha-response'] ) ) {
+				$response = $this->_form->_submitValues['g-recaptcha-response'];
+				if ( ! $recaptchaelement->verify( $response ) ) {
+					$errors['recaptcha_element'] = get_string( 'incorrectpleasetryagain', 'auth' );
+				}
+			} else {
+				$errors['recaptcha_element'] = get_string( 'missingrecaptchachallengefield' );
+			}
+		}
 
-        if (signup_captcha_enabled()) {
-            $recaptchaelement = $this->_form->getElement('recaptcha_element');
-            if (!empty($this->_form->_submitValues['g-recaptcha-response'])) {
-                $response = $this->_form->_submitValues['g-recaptcha-response'];
-                if (!$recaptchaelement->verify($response)) {
-                    $errors['recaptcha_element'] = get_string('incorrectpleasetryagain', 'auth');
-                }
-            } else {
-                $errors['recaptcha_element'] = get_string('missingrecaptchachallengefield');
-            }
-        }
+		$errors += signup_validate_data( $data, $files );
 
-        $errors += signup_validate_data($data, $files);
+		return $errors;
+	}
 
-        return $errors;
-    }
+	/**
+	 * Export this data so it can be used as the context for a mustache template.
+	 *
+	 * @param renderer_base $output Used to do a final render of any components that need to be rendered for export.
+	 *
+	 * @return array
+	 */
+	public function export_for_template( renderer_base $output ) {
+		ob_start();
+		$this->display();
+		$formhtml = ob_get_contents();
+		ob_end_clean();
+		$context = [
+			'formhtml' => $formhtml
+		];
 
-    /**
-     * Export this data so it can be used as the context for a mustache template.
-     *
-     * @param renderer_base $output Used to do a final render of any components that need to be rendered for export.
-     * @return array
-     */
-    public function export_for_template(renderer_base $output) {
-        ob_start();
-        $this->display();
-        $formhtml = ob_get_contents();
-        ob_end_clean();
-        $context = [
-            'formhtml' => $formhtml
-        ];
-        return $context;
-    }
+		return $context;
+	}
 }
